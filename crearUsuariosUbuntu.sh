af04cd018b91b4fa8837485c83ff5542fa6c2955
claudiavilgra <clavilgra@alu.edu.gva.es>
2025-12-21 22:25:44 +0100

    Update crearUsuariosUbuntu.sh

--- crearUsuariosUbuntu.sh
+++ crearUsuariosUbuntu.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-while IFS=, read cuenta password descripcion nombre_apellidos grupo_departamento  
+
+tail -n +2 usuariosUbuntu.csv | while IFS=, read -r cuenta password descripcion nombre_apellidos grupo_departamento
 do
-  echo "cuenta      -> [${cuenta}]"
-  echo "password      -> [${password}]"
-  echo "descripcion      -> [${descripcion}]"
-  echo "nombre_apellidos      -> [${nombre_apellidos}]"
-  echo "grupo_departamento      -> [${grupo_departamento}]"
- 
-done < <(tail -n +2 usuariosUbuntu.csv)
+  sudo useradd -m -g "$grupo_departamento" -c "$nombre_apellidos - $descripcion" "$cuenta"
+  echo "$cuenta:$password" | sudo chpasswd
+  echo "Usuario '$cuenta' creado y asignado al grupo '$grupo_departamento'"
+done
